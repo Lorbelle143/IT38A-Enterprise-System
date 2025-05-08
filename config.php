041b57e1aad8a96bba5a6ec239b935c2fce61691
Lorbelle143 <lorbelleganzan@gmail.com>
2025-05-08 18:53:41 +0800

    updated config

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";  // Default username for XAMPP is "root"
 $password = "";      // Default password for XAMPP is empty
-$dbname = "IT38a-Enterprise-System";  // The database name you created
+$dbname = "IT38A-Enterprise-System";  // The database name you created
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
